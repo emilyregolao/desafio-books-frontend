07c7f4dfc241769b1cbeead5251c2e3201c6e75d
Emily <emregolao@gmail.com>
2021-12-11 17:32:51 -0300

    build(package.json):removes unused librearies

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Providers from "./Providers/index";
 import { BrowserRouter } from "react-router-dom";
 
@@ -15,4 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-reportWebVitals();
